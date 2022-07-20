3af58f408dec30b5e111b95eb5e265ad1b19ca5e
fxnt <1330672764@qq.com>
2022-07-20 11:58:41 +0800

    add app middleware

---
+++ npool/appusergateway/app/errmsg.go
@@ -0,0 +1,10 @@
+package app
+
+const (
+	ErrMsgAppAlreadyExists     = "AppAlreadyExists"
+	ErrMsgAppNameAlreadyExists = "AppNameAlreadyExists"
+	ErrMsgAppCreatedByInvalid  = "AppCreatedByInvalid"
+	ErrMsgAppNameEmpty         = "AppNameEmpty"
+	ErrMsgAppLogoEmpty         = "AppLogoEmpty"
+	ErrMsgAppIDInvalid         = "AppIDInvalid"
+)
---
+++ npool/errcode.go
@@ -0,0 +1,11 @@
+package npool
+
+const (
+	ErrService = 10000 + iota
+	ErrParams
+	ErrAlreadyExists
+)
+
+const (
+	ErrMsgServiceErr = "ServiceErr"
+)
