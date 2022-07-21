a0b490f8b4147db3535fc4adebe01021149aaa02
fxnt <1330672764@qq.com>
2022-07-21 14:37:29 +0800

    add infos

--- npool/sphinxproxy/sphinxproxy.pb.gw.go
+++ npool/sphinxproxy/sphinxproxy.pb.gw.go
@@ -270,8 +270,47 @@ func local_request_SphinxProxy_GetTransactions_0(ctx context.Context, marshaler 
 
 }
 
+<<<<<<< HEAD:npool/sphinxproxy/sphinxproxy.pb.gw.go
 // RegisterSphinxProxyHandlerServer registers the http handlers for service SphinxProxy to "mux".
 // UnaryRPC     :call SphinxProxyServer directly.
+=======
+func request_AppUserGatewayApp_GetAppInfos_0(ctx context.Context, marshaler runtime.Marshaler, client AppUserGatewayAppClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetAppInfosRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetAppInfos(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_AppUserGatewayApp_GetAppInfos_0(ctx context.Context, marshaler runtime.Marshaler, server AppUserGatewayAppServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetAppInfosRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetAppInfos(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+// RegisterAppUserGatewayAppHandlerServer registers the http handlers for service AppUserGatewayApp to "mux".
+// UnaryRPC     :call AppUserGatewayAppServer directly.
+>>>>>>> add infos:npool/appusergateway/app/app.pb.gw.go
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterSphinxProxyHandlerFromEndpoint instead.
 func RegisterSphinxProxyHandlerServer(ctx context.Context, mux *runtime.ServeMux, server SphinxProxyServer) error {
@@ -434,6 +473,29 @@ func RegisterSphinxProxyHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_SphinxProxy_GetTransactions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_AppUserGatewayApp_GetAppInfos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/app.user.gateway.app.v2.AppUserGatewayApp/GetAppInfos", runtime.WithHTTPPathPattern("/v2/get/app/infos"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_AppUserGatewayApp_GetAppInfos_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AppUserGatewayApp_GetAppInfos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -618,6 +680,26 @@ func RegisterSphinxProxyHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_AppUserGatewayApp_GetAppInfos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/app.user.gateway.app.v2.AppUserGatewayApp/GetAppInfos", runtime.WithHTTPPathPattern("/v2/get/app/infos"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AppUserGatewayApp_GetAppInfos_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AppUserGatewayApp_GetAppInfos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -634,7 +716,13 @@ var (
 
 	pattern_SphinxProxy_GetTransaction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "get", "transaction"}, ""))
 
+<<<<<<< HEAD:npool/sphinxproxy/sphinxproxy.pb.gw.go
 	pattern_SphinxProxy_GetTransactions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "get", "transactions"}, ""))
+=======
+	pattern_AppUserGatewayApp_GetAppInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v2", "get", "app", "info"}, ""))
+
+	pattern_AppUserGatewayApp_GetAppInfos_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v2", "get", "app", "infos"}, ""))
+>>>>>>> add infos:npool/appusergateway/app/app.pb.gw.go
 )
 
 var (
@@ -650,5 +738,11 @@ var (
 
 	forward_SphinxProxy_GetTransaction_0 = runtime.ForwardResponseMessage
 
+<<<<<<< HEAD:npool/sphinxproxy/sphinxproxy.pb.gw.go
 	forward_SphinxProxy_GetTransactions_0 = runtime.ForwardResponseMessage
+=======
+	forward_AppUserGatewayApp_GetAppInfo_0 = runtime.ForwardResponseMessage
+
+	forward_AppUserGatewayApp_GetAppInfos_0 = runtime.ForwardResponseMessage
+>>>>>>> add infos:npool/appusergateway/app/app.pb.gw.go
 )
