e36a325f4eef78f0e49876db74f74cb246ee2ac7
Zhao KK <kikakkz@hotmail.com>
2022-07-29 11:33:48 +0000

    Clean unused code

--- npool/appuser/mw/v1/app/app_grpc.pb.go
+++ npool/appuser/mw/v1/app/app_grpc.pb.go
@@ -3,15 +3,6 @@
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.18.1
 // source: npool/appuser/mw/v1/app/app.proto
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// source: npool/appusermw/app/app.proto
->>>>>>> basically completed new apis:npool/appusermw/app/app_grpc.pb.go
-=======
->>>>>>> Add enum ERR
-=======
->>>>>>> e88957ae27002b487b67d1e48d81519801cf6097
 
 package app
 
@@ -543,13 +534,4 @@ var AppMw_ServiceDesc = grpc.ServiceDesc{
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "npool/appuser/mw/v1/app/app.proto",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	Metadata: "npool/appusermw/app/app.proto",
->>>>>>> basically completed new apis:npool/appusermw/app/app_grpc.pb.go
-=======
->>>>>>> Add enum ERR
-=======
->>>>>>> e88957ae27002b487b67d1e48d81519801cf6097
 }
