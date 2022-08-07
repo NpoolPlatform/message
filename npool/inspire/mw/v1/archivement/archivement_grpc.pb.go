12601ac5bc88dd1668dcd0d3f8abf13b922d970b
Zhao KK <kikakkz@hotmail.com>
2022-08-07 15:48:34 +0000

    Add archivement book keeping

--- npool/inspire/mw/v1/archivement/archivement_grpc.pb.go
+++ npool/inspire/mw/v1/archivement/archivement_grpc.pb.go
@@ -8,11 +8,9 @@ package archivement
 
 import (
 	context "context"
-	npool "github.com/NpoolPlatform/message/npool"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -24,8 +22,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type MiddlewareClient interface {
-	// Method Version
-	Version(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*npool.VersionResponse, error)
+	BookKeeping(ctx context.Context, in *BookKeepingRequest, opts ...grpc.CallOption) (*BookKeepingResponse, error)
 }
 
 type middlewareClient struct {
@@ -36,9 +33,9 @@ func NewMiddlewareClient(cc grpc.ClientConnInterface) MiddlewareClient {
 	return &middlewareClient{cc}
 }
 
-func (c *middlewareClient) Version(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*npool.VersionResponse, error) {
-	out := new(npool.VersionResponse)
-	err := c.cc.Invoke(ctx, "/inspire.middleware.archivement.v1.Middleware/Version", in, out, opts...)
+func (c *middlewareClient) BookKeeping(ctx context.Context, in *BookKeepingRequest, opts ...grpc.CallOption) (*BookKeepingResponse, error) {
+	out := new(BookKeepingResponse)
+	err := c.cc.Invoke(ctx, "/inspire.middleware.archivement.v1.Middleware/BookKeeping", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -49,8 +46,7 @@ func (c *middlewareClient) Version(ctx context.Context, in *emptypb.Empty, opts 
 // All implementations must embed UnimplementedMiddlewareServer
 // for forward compatibility
 type MiddlewareServer interface {
-	// Method Version
-	Version(context.Context, *emptypb.Empty) (*npool.VersionResponse, error)
+	BookKeeping(context.Context, *BookKeepingRequest) (*BookKeepingResponse, error)
 	mustEmbedUnimplementedMiddlewareServer()
 }
 
@@ -58,8 +54,8 @@ type MiddlewareServer interface {
 type UnimplementedMiddlewareServer struct {
 }
 
-func (UnimplementedMiddlewareServer) Version(context.Context, *emptypb.Empty) (*npool.VersionResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Version not implemented")
+func (UnimplementedMiddlewareServer) BookKeeping(context.Context, *BookKeepingRequest) (*BookKeepingResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method BookKeeping not implemented")
 }
 func (UnimplementedMiddlewareServer) mustEmbedUnimplementedMiddlewareServer() {}
 
@@ -74,20 +70,20 @@ func RegisterMiddlewareServer(s grpc.ServiceRegistrar, srv MiddlewareServer) {
 	s.RegisterService(&Middleware_ServiceDesc, srv)
 }
 
-func _Middleware_Version_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(emptypb.Empty)
+func _Middleware_BookKeeping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BookKeepingRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(MiddlewareServer).Version(ctx, in)
+		return srv.(MiddlewareServer).BookKeeping(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/inspire.middleware.archivement.v1.Middleware/Version",
+		FullMethod: "/inspire.middleware.archivement.v1.Middleware/BookKeeping",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).Version(ctx, req.(*emptypb.Empty))
+		return srv.(MiddlewareServer).BookKeeping(ctx, req.(*BookKeepingRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -100,8 +96,8 @@ var Middleware_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*MiddlewareServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Version",
-			Handler:    _Middleware_Version_Handler,
+			MethodName: "BookKeeping",
+			Handler:    _Middleware_BookKeeping_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
