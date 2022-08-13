35653d64ff3d6eb27f0c9cfbdc77c5178e1f065d
Zhao KK <kikakkz@hotmail.com>
2022-08-13 08:06:11 +0000

    Add get archivements api

--- npool/inspire/gw/v1/archivement/archivement_grpc.pb.go
+++ npool/inspire/gw/v1/archivement/archivement_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type GatewayClient interface {
 	GetGoodArchivements(ctx context.Context, in *GetGoodArchivementsRequest, opts ...grpc.CallOption) (*GetGoodArchivementsResponse, error)
+	GetUserGoodArchivements(ctx context.Context, in *GetUserGoodArchivementsRequest, opts ...grpc.CallOption) (*GetUserGoodArchivementsResponse, error)
 }
 
 type gatewayClient struct {
@@ -42,11 +43,21 @@ func (c *gatewayClient) GetGoodArchivements(ctx context.Context, in *GetGoodArch
 	return out, nil
 }
 
+func (c *gatewayClient) GetUserGoodArchivements(ctx context.Context, in *GetUserGoodArchivementsRequest, opts ...grpc.CallOption) (*GetUserGoodArchivementsResponse, error) {
+	out := new(GetUserGoodArchivementsResponse)
+	err := c.cc.Invoke(ctx, "/inspire.gateway.archivement.v1.Gateway/GetUserGoodArchivements", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GatewayServer is the server API for Gateway service.
 // All implementations must embed UnimplementedGatewayServer
 // for forward compatibility
 type GatewayServer interface {
 	GetGoodArchivements(context.Context, *GetGoodArchivementsRequest) (*GetGoodArchivementsResponse, error)
+	GetUserGoodArchivements(context.Context, *GetUserGoodArchivementsRequest) (*GetUserGoodArchivementsResponse, error)
 	mustEmbedUnimplementedGatewayServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedGatewayServer struct {
 
 func (UnimplementedGatewayServer) GetGoodArchivements(context.Context, *GetGoodArchivementsRequest) (*GetGoodArchivementsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetGoodArchivements not implemented")
+}
+func (UnimplementedGatewayServer) GetUserGoodArchivements(context.Context, *GetUserGoodArchivementsRequest) (*GetUserGoodArchivementsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUserGoodArchivements not implemented")
 }
 func (UnimplementedGatewayServer) mustEmbedUnimplementedGatewayServer() {}
 
@@ -88,6 +102,24 @@ func _Gateway_GetGoodArchivements_Handler(srv interface{}, ctx context.Context, 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Gateway_GetUserGoodArchivements_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetUserGoodArchivementsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GatewayServer).GetUserGoodArchivements(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/inspire.gateway.archivement.v1.Gateway/GetUserGoodArchivements",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GatewayServer).GetUserGoodArchivements(ctx, req.(*GetUserGoodArchivementsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Gateway_ServiceDesc is the grpc.ServiceDesc for Gateway service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var Gateway_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetGoodArchivements",
 			Handler:    _Gateway_GetGoodArchivements_Handler,
+		},
+		{
+			MethodName: "GetUserGoodArchivements",
+			Handler:    _Gateway_GetUserGoodArchivements_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
