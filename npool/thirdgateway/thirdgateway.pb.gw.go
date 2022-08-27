3349ff3f3b43cd1bc530e83bb19657c0906936df
Zhao KK <kikakkz@hotmail.com>
2022-08-27 16:23:19 +0000

    Remove google authentication

--- npool/thirdgateway/thirdgateway.pb.gw.go
+++ npool/thirdgateway/thirdgateway.pb.gw.go
@@ -916,74 +916,6 @@ func local_request_ThirdGateway_VerifyEmailCode_0(ctx context.Context, marshaler
 
 }
 
-func request_ThirdGateway_SetupGoogleAuthentication_0(ctx context.Context, marshaler runtime.Marshaler, client ThirdGatewayClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq SetupGoogleAuthenticationRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.SetupGoogleAuthentication(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_ThirdGateway_SetupGoogleAuthentication_0(ctx context.Context, marshaler runtime.Marshaler, server ThirdGatewayServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq SetupGoogleAuthenticationRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.SetupGoogleAuthentication(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
-func request_ThirdGateway_VerifyGoogleAuthentication_0(ctx context.Context, marshaler runtime.Marshaler, client ThirdGatewayClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VerifyGoogleAuthenticationRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.VerifyGoogleAuthentication(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_ThirdGateway_VerifyGoogleAuthentication_0(ctx context.Context, marshaler runtime.Marshaler, server ThirdGatewayServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VerifyGoogleAuthenticationRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.VerifyGoogleAuthentication(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 func request_ThirdGateway_VerifyGoogleRecaptchaV3_0(ctx context.Context, marshaler runtime.Marshaler, client ThirdGatewayClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq VerifyGoogleRecaptchaV3Request
 	var metadata runtime.ServerMetadata
@@ -1690,52 +1622,6 @@ func RegisterThirdGatewayHandlerServer(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("POST", pattern_ThirdGateway_SetupGoogleAuthentication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/third.gateway.v1.ThirdGateway/SetupGoogleAuthentication", runtime.WithHTTPPathPattern("/v1/setup/google/authentication"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_ThirdGateway_SetupGoogleAuthentication_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ThirdGateway_SetupGoogleAuthentication_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("POST", pattern_ThirdGateway_VerifyGoogleAuthentication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/third.gateway.v1.ThirdGateway/VerifyGoogleAuthentication", runtime.WithHTTPPathPattern("/v1/verify/google/authentication"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_ThirdGateway_VerifyGoogleAuthentication_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ThirdGateway_VerifyGoogleAuthentication_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("POST", pattern_ThirdGateway_VerifyGoogleRecaptchaV3_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2366,46 +2252,6 @@ func RegisterThirdGatewayHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("POST", pattern_ThirdGateway_SetupGoogleAuthentication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/third.gateway.v1.ThirdGateway/SetupGoogleAuthentication", runtime.WithHTTPPathPattern("/v1/setup/google/authentication"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_ThirdGateway_SetupGoogleAuthentication_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ThirdGateway_SetupGoogleAuthentication_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("POST", pattern_ThirdGateway_VerifyGoogleAuthentication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/third.gateway.v1.ThirdGateway/VerifyGoogleAuthentication", runtime.WithHTTPPathPattern("/v1/verify/google/authentication"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_ThirdGateway_VerifyGoogleAuthentication_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_ThirdGateway_VerifyGoogleAuthentication_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("POST", pattern_ThirdGateway_VerifyGoogleRecaptchaV3_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2522,10 +2368,6 @@ var (
 
 	pattern_ThirdGateway_VerifyEmailCode_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "verify", "email", "code"}, ""))
 
-	pattern_ThirdGateway_SetupGoogleAuthentication_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "setup", "google", "authentication"}, ""))
-
-	pattern_ThirdGateway_VerifyGoogleAuthentication_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "verify", "google", "authentication"}, ""))
-
 	pattern_ThirdGateway_VerifyGoogleRecaptchaV3_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "verify", "google", "recaptcha", "v3"}, ""))
 
 	pattern_ThirdGateway_ContactByEmail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "contact", "by", "email"}, ""))
@@ -2585,10 +2427,6 @@ var (
 	forward_ThirdGateway_SendEmailCode_0 = runtime.ForwardResponseMessage
 
 	forward_ThirdGateway_VerifyEmailCode_0 = runtime.ForwardResponseMessage
-
-	forward_ThirdGateway_SetupGoogleAuthentication_0 = runtime.ForwardResponseMessage
-
-	forward_ThirdGateway_VerifyGoogleAuthentication_0 = runtime.ForwardResponseMessage
 
 	forward_ThirdGateway_VerifyGoogleRecaptchaV3_0 = runtime.ForwardResponseMessage
 
