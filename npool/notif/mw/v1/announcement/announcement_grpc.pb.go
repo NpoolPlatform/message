ba03a9b7c42b8dbe45d875bd6a89e4092dce7c41
JobNing <q178149@163.com>
2023-02-08 13:10:51 +0800

    add announcement states api

--- npool/notif/mw/v1/announcement/announcement_grpc.pb.go
+++ npool/notif/mw/v1/announcement/announcement_grpc.pb.go
@@ -22,6 +22,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type MiddlewareClient interface {
+	GetAnnouncementStates(ctx context.Context, in *GetAnnouncementStatesRequest, opts ...grpc.CallOption) (*GetAnnouncementStatesResponse, error)
 	GetAnnouncements(ctx context.Context, in *GetAnnouncementsRequest, opts ...grpc.CallOption) (*GetAnnouncementsResponse, error)
 }
 
@@ -31,6 +32,15 @@ type middlewareClient struct {
 
 func NewMiddlewareClient(cc grpc.ClientConnInterface) MiddlewareClient {
 	return &middlewareClient{cc}
+}
+
+func (c *middlewareClient) GetAnnouncementStates(ctx context.Context, in *GetAnnouncementStatesRequest, opts ...grpc.CallOption) (*GetAnnouncementStatesResponse, error) {
+	out := new(GetAnnouncementStatesResponse)
+	err := c.cc.Invoke(ctx, "/notif.middleware.announcement.announcement.v1.Middleware/GetAnnouncementStates", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *middlewareClient) GetAnnouncements(ctx context.Context, in *GetAnnouncementsRequest, opts ...grpc.CallOption) (*GetAnnouncementsResponse, error) {
@@ -46,6 +56,7 @@ func (c *middlewareClient) GetAnnouncements(ctx context.Context, in *GetAnnounce
 // All implementations must embed UnimplementedMiddlewareServer
 // for forward compatibility
 type MiddlewareServer interface {
+	GetAnnouncementStates(context.Context, *GetAnnouncementStatesRequest) (*GetAnnouncementStatesResponse, error)
 	GetAnnouncements(context.Context, *GetAnnouncementsRequest) (*GetAnnouncementsResponse, error)
 	mustEmbedUnimplementedMiddlewareServer()
 }
@@ -54,6 +65,9 @@ type MiddlewareServer interface {
 type UnimplementedMiddlewareServer struct {
 }
 
+func (UnimplementedMiddlewareServer) GetAnnouncementStates(context.Context, *GetAnnouncementStatesRequest) (*GetAnnouncementStatesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAnnouncementStates not implemented")
+}
 func (UnimplementedMiddlewareServer) GetAnnouncements(context.Context, *GetAnnouncementsRequest) (*GetAnnouncementsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAnnouncements not implemented")
 }
@@ -68,6 +82,24 @@ type UnsafeMiddlewareServer interface {
 
 func RegisterMiddlewareServer(s grpc.ServiceRegistrar, srv MiddlewareServer) {
 	s.RegisterService(&Middleware_ServiceDesc, srv)
+}
+
+func _Middleware_GetAnnouncementStates_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetAnnouncementStatesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MiddlewareServer).GetAnnouncementStates(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/notif.middleware.announcement.announcement.v1.Middleware/GetAnnouncementStates",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MiddlewareServer).GetAnnouncementStates(ctx, req.(*GetAnnouncementStatesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _Middleware_GetAnnouncements_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -95,6 +127,10 @@ var Middleware_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "notif.middleware.announcement.announcement.v1.Middleware",
 	HandlerType: (*MiddlewareServer)(nil),
 	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetAnnouncementStates",
+			Handler:    _Middleware_GetAnnouncementStates_Handler,
+		},
 		{
 			MethodName: "GetAnnouncements",
 			Handler:    _Middleware_GetAnnouncements_Handler,
