bce3f87f8cf9d4f4818714f9a223e6c4a085ee7c
daqiangli <dqli161@163.com>
2023-04-24 10:50:30 +0800

    add ExistModule

--- npool/smoketest/mw/v1/module/module_grpc.pb.go
+++ npool/smoketest/mw/v1/module/module_grpc.pb.go
@@ -27,6 +27,7 @@ type MiddlewareClient interface {
 	GetModules(ctx context.Context, in *GetModulesRequest, opts ...grpc.CallOption) (*GetModulesResponse, error)
 	GetModule(ctx context.Context, in *GetModuleRequest, opts ...grpc.CallOption) (*GetModuleResponse, error)
 	DeleteModule(ctx context.Context, in *DeleteModuleRequest, opts ...grpc.CallOption) (*DeleteModuleResponse, error)
+	ExistModule(ctx context.Context, in *ExistModuleRequest, opts ...grpc.CallOption) (*ExistModuleResponse, error)
 	ExistModuleByName(ctx context.Context, in *ExistModuleByNameRequest, opts ...grpc.CallOption) (*ExistModuleByNameResponse, error)
 	ExistModuleConds(ctx context.Context, in *ExistModuleCondsRequest, opts ...grpc.CallOption) (*ExistModuleCondsResponse, error)
 }
@@ -84,6 +85,15 @@ func (c *middlewareClient) DeleteModule(ctx context.Context, in *DeleteModuleReq
 	return out, nil
 }
 
+func (c *middlewareClient) ExistModule(ctx context.Context, in *ExistModuleRequest, opts ...grpc.CallOption) (*ExistModuleResponse, error) {
+	out := new(ExistModuleResponse)
+	err := c.cc.Invoke(ctx, "/smoketest.middleware.module.v1.Middleware/ExistModule", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *middlewareClient) ExistModuleByName(ctx context.Context, in *ExistModuleByNameRequest, opts ...grpc.CallOption) (*ExistModuleByNameResponse, error) {
 	out := new(ExistModuleByNameResponse)
 	err := c.cc.Invoke(ctx, "/smoketest.middleware.module.v1.Middleware/ExistModuleByName", in, out, opts...)
@@ -111,6 +121,7 @@ type MiddlewareServer interface {
 	GetModules(context.Context, *GetModulesRequest) (*GetModulesResponse, error)
 	GetModule(context.Context, *GetModuleRequest) (*GetModuleResponse, error)
 	DeleteModule(context.Context, *DeleteModuleRequest) (*DeleteModuleResponse, error)
+	ExistModule(context.Context, *ExistModuleRequest) (*ExistModuleResponse, error)
 	ExistModuleByName(context.Context, *ExistModuleByNameRequest) (*ExistModuleByNameResponse, error)
 	ExistModuleConds(context.Context, *ExistModuleCondsRequest) (*ExistModuleCondsResponse, error)
 	mustEmbedUnimplementedMiddlewareServer()
@@ -134,6 +145,9 @@ func (UnimplementedMiddlewareServer) GetModule(context.Context, *GetModuleReques
 }
 func (UnimplementedMiddlewareServer) DeleteModule(context.Context, *DeleteModuleRequest) (*DeleteModuleResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteModule not implemented")
+}
+func (UnimplementedMiddlewareServer) ExistModule(context.Context, *ExistModuleRequest) (*ExistModuleResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ExistModule not implemented")
 }
 func (UnimplementedMiddlewareServer) ExistModuleByName(context.Context, *ExistModuleByNameRequest) (*ExistModuleByNameResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExistModuleByName not implemented")
@@ -244,6 +258,24 @@ func _Middleware_DeleteModule_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Middleware_ExistModule_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ExistModuleRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MiddlewareServer).ExistModule(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/smoketest.middleware.module.v1.Middleware/ExistModule",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MiddlewareServer).ExistModule(ctx, req.(*ExistModuleRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _Middleware_ExistModuleByName_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ExistModuleByNameRequest)
 	if err := dec(in); err != nil {
@@ -306,6 +338,10 @@ var Middleware_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "DeleteModule",
 			Handler:    _Middleware_DeleteModule_Handler,
+		},
+		{
+			MethodName: "ExistModule",
+			Handler:    _Middleware_ExistModule_Handler,
 		},
 		{
 			MethodName: "ExistModuleByName",
