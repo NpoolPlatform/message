e065eda26065229ec76d15b0c5eea25cb26ac57d
Zhao KK <kikakkz@hotmail.com>
2023-05-27 02:20:21 +0000

    Use right tag

--- npool/chain/mw/v1/fiat/currency/feed/feed.pb.go
+++ npool/chain/mw/v1/fiat/currency/feed/feed.pb.go
@@ -136,8 +136,8 @@ type Feed struct {
 	// @inject_tag: sql:"feed_type"
 	FeedTypeStr string              `protobuf:"bytes,60,opt,name=FeedTypeStr,proto3" json:"FeedTypeStr,omitempty" sql:"feed_type"`
 	FeedType    v1.CurrencyFeedType `protobuf:"varint,70,opt,name=FeedType,proto3,enum=basetypes.v1.CurrencyFeedType" json:"FeedType,omitempty"`
-	// @inject_tag: sql:"feed_coin_name"
-	FeedFiatName string `protobuf:"bytes,80,opt,name=FeedFiatName,proto3" json:"FeedFiatName,omitempty" sql:"feed_coin_name"`
+	// @inject_tag: sql:"feed_fiat_name"
+	FeedFiatName string `protobuf:"bytes,80,opt,name=FeedFiatName,proto3" json:"FeedFiatName,omitempty" sql:"feed_fiat_name"`
 	// @inject_tag: sql:"disabled"
 <<<<<<< HEAD
 	Disabled bool `protobuf:"varint,60,opt,name=Disabled,proto3" json:"Disabled,omitempty" sql:"disabled"`
