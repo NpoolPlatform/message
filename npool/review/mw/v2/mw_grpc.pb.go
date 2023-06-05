0caefeb7b3ac3045d39fdd7a6942798a0ca5c2e4
daqiangli <dqli161@163.com>
2023-06-05 16:37:55 +0800

    change structure

--- npool/review/mw/v2/mw_grpc.pb.go
+++ npool/review/mw/v2/mw_grpc.pb.go
@@ -8,9 +8,11 @@ package v2
 
 import (
 	context "context"
+	npool "github.com/NpoolPlatform/message/npool"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,26 +21,14 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Middleware_CreateReview_FullMethodName     = "/review.middleware.v2.Middleware/CreateReview"
-	Middleware_UpdateReview_FullMethodName     = "/review.middleware.v2.Middleware/UpdateReview"
-	Middleware_GetObjectReview_FullMethodName  = "/review.middleware.v2.Middleware/GetObjectReview"
-	Middleware_GetObjectReviews_FullMethodName = "/review.middleware.v2.Middleware/GetObjectReviews"
-	Middleware_GetReviews_FullMethodName       = "/review.middleware.v2.Middleware/GetReviews"
-	Middleware_DeleteReview_FullMethodName     = "/review.middleware.v2.Middleware/DeleteReview"
-	Middleware_GetReview_FullMethodName        = "/review.middleware.v2.Middleware/GetReview"
+	Middleware_Version_FullMethodName = "/review.middleware.v2.Middleware/Version"
 )
 
 // MiddlewareClient is the client API for Middleware service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type MiddlewareClient interface {
-	CreateReview(ctx context.Context, in *CreateReviewRequest, opts ...grpc.CallOption) (*CreateReviewResponse, error)
-	UpdateReview(ctx context.Context, in *UpdateReviewRequest, opts ...grpc.CallOption) (*UpdateReviewResponse, error)
-	GetObjectReview(ctx context.Context, in *GetObjectReviewRequest, opts ...grpc.CallOption) (*GetObjectReviewResponse, error)
-	GetObjectReviews(ctx context.Context, in *GetObjectReviewsRequest, opts ...grpc.CallOption) (*GetObjectReviewsResponse, error)
-	GetReviews(ctx context.Context, in *GetReviewsRequest, opts ...grpc.CallOption) (*GetReviewsResponse, error)
-	DeleteReview(ctx context.Context, in *DeleteReviewRequest, opts ...grpc.CallOption) (*DeleteReviewResponse, error)
-	GetReview(ctx context.Context, in *GetReviewRequest, opts ...grpc.CallOption) (*GetReviewResponse, error)
+	Version(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*npool.VersionResponse, error)
 }
 
 type middlewareClient struct {
@@ -49,63 +39,9 @@ func NewMiddlewareClient(cc grpc.ClientConnInterface) MiddlewareClient {
 	return &middlewareClient{cc}
 }
 
-func (c *middlewareClient) CreateReview(ctx context.Context, in *CreateReviewRequest, opts ...grpc.CallOption) (*CreateReviewResponse, error) {
-	out := new(CreateReviewResponse)
-	err := c.cc.Invoke(ctx, Middleware_CreateReview_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) UpdateReview(ctx context.Context, in *UpdateReviewRequest, opts ...grpc.CallOption) (*UpdateReviewResponse, error) {
-	out := new(UpdateReviewResponse)
-	err := c.cc.Invoke(ctx, Middleware_UpdateReview_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) GetObjectReview(ctx context.Context, in *GetObjectReviewRequest, opts ...grpc.CallOption) (*GetObjectReviewResponse, error) {
-	out := new(GetObjectReviewResponse)
-	err := c.cc.Invoke(ctx, Middleware_GetObjectReview_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) GetObjectReviews(ctx context.Context, in *GetObjectReviewsRequest, opts ...grpc.CallOption) (*GetObjectReviewsResponse, error) {
-	out := new(GetObjectReviewsResponse)
-	err := c.cc.Invoke(ctx, Middleware_GetObjectReviews_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) GetReviews(ctx context.Context, in *GetReviewsRequest, opts ...grpc.CallOption) (*GetReviewsResponse, error) {
-	out := new(GetReviewsResponse)
-	err := c.cc.Invoke(ctx, Middleware_GetReviews_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) DeleteReview(ctx context.Context, in *DeleteReviewRequest, opts ...grpc.CallOption) (*DeleteReviewResponse, error) {
-	out := new(DeleteReviewResponse)
-	err := c.cc.Invoke(ctx, Middleware_DeleteReview_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *middlewareClient) GetReview(ctx context.Context, in *GetReviewRequest, opts ...grpc.CallOption) (*GetReviewResponse, error) {
-	out := new(GetReviewResponse)
-	err := c.cc.Invoke(ctx, Middleware_GetReview_FullMethodName, in, out, opts...)
+func (c *middlewareClient) Version(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*npool.VersionResponse, error) {
+	out := new(npool.VersionResponse)
+	err := c.cc.Invoke(ctx, Middleware_Version_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -116,13 +52,7 @@ func (c *middlewareClient) GetReview(ctx context.Context, in *GetReviewRequest, 
 // All implementations must embed UnimplementedMiddlewareServer
 // for forward compatibility
 type MiddlewareServer interface {
-	CreateReview(context.Context, *CreateReviewRequest) (*CreateReviewResponse, error)
-	UpdateReview(context.Context, *UpdateReviewRequest) (*UpdateReviewResponse, error)
-	GetObjectReview(context.Context, *GetObjectReviewRequest) (*GetObjectReviewResponse, error)
-	GetObjectReviews(context.Context, *GetObjectReviewsRequest) (*GetObjectReviewsResponse, error)
-	GetReviews(context.Context, *GetReviewsRequest) (*GetReviewsResponse, error)
-	DeleteReview(context.Context, *DeleteReviewRequest) (*DeleteReviewResponse, error)
-	GetReview(context.Context, *GetReviewRequest) (*GetReviewResponse, error)
+	Version(context.Context, *emptypb.Empty) (*npool.VersionResponse, error)
 	mustEmbedUnimplementedMiddlewareServer()
 }
 
@@ -130,26 +60,8 @@ type MiddlewareServer interface {
 type UnimplementedMiddlewareServer struct {
 }
 
-func (UnimplementedMiddlewareServer) CreateReview(context.Context, *CreateReviewRequest) (*CreateReviewResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateReview not implemented")
-}
-func (UnimplementedMiddlewareServer) UpdateReview(context.Context, *UpdateReviewRequest) (*UpdateReviewResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateReview not implemented")
-}
-func (UnimplementedMiddlewareServer) GetObjectReview(context.Context, *GetObjectReviewRequest) (*GetObjectReviewResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetObjectReview not implemented")
-}
-func (UnimplementedMiddlewareServer) GetObjectReviews(context.Context, *GetObjectReviewsRequest) (*GetObjectReviewsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetObjectReviews not implemented")
-}
-func (UnimplementedMiddlewareServer) GetReviews(context.Context, *GetReviewsRequest) (*GetReviewsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetReviews not implemented")
-}
-func (UnimplementedMiddlewareServer) DeleteReview(context.Context, *DeleteReviewRequest) (*DeleteReviewResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DeleteReview not implemented")
-}
-func (UnimplementedMiddlewareServer) GetReview(context.Context, *GetReviewRequest) (*GetReviewResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetReview not implemented")
+func (UnimplementedMiddlewareServer) Version(context.Context, *emptypb.Empty) (*npool.VersionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Version not implemented")
 }
 func (UnimplementedMiddlewareServer) mustEmbedUnimplementedMiddlewareServer() {}
 
@@ -164,128 +76,20 @@ func RegisterMiddlewareServer(s grpc.ServiceRegistrar, srv MiddlewareServer) {
 	s.RegisterService(&Middleware_ServiceDesc, srv)
 }
 
-func _Middleware_CreateReview_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateReviewRequest)
+func _Middleware_Version_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(MiddlewareServer).CreateReview(ctx, in)
+		return srv.(MiddlewareServer).Version(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Middleware_CreateReview_FullMethodName,
+		FullMethod: Middleware_Version_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).CreateReview(ctx, req.(*CreateReviewRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_UpdateReview_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UpdateReviewRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).UpdateReview(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_UpdateReview_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).UpdateReview(ctx, req.(*UpdateReviewRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_GetObjectReview_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetObjectReviewRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).GetObjectReview(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_GetObjectReview_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).GetObjectReview(ctx, req.(*GetObjectReviewRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_GetObjectReviews_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetObjectReviewsRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).GetObjectReviews(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_GetObjectReviews_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).GetObjectReviews(ctx, req.(*GetObjectReviewsRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_GetReviews_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetReviewsRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).GetReviews(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_GetReviews_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).GetReviews(ctx, req.(*GetReviewsRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_DeleteReview_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DeleteReviewRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).DeleteReview(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_DeleteReview_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).DeleteReview(ctx, req.(*DeleteReviewRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Middleware_GetReview_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetReviewRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).GetReview(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_GetReview_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).GetReview(ctx, req.(*GetReviewRequest))
+		return srv.(MiddlewareServer).Version(ctx, req.(*emptypb.Empty))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -298,32 +102,8 @@ var Middleware_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*MiddlewareServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "CreateReview",
-			Handler:    _Middleware_CreateReview_Handler,
-		},
-		{
-			MethodName: "UpdateReview",
-			Handler:    _Middleware_UpdateReview_Handler,
-		},
-		{
-			MethodName: "GetObjectReview",
-			Handler:    _Middleware_GetObjectReview_Handler,
-		},
-		{
-			MethodName: "GetObjectReviews",
-			Handler:    _Middleware_GetObjectReviews_Handler,
-		},
-		{
-			MethodName: "GetReviews",
-			Handler:    _Middleware_GetReviews_Handler,
-		},
-		{
-			MethodName: "DeleteReview",
-			Handler:    _Middleware_DeleteReview_Handler,
-		},
-		{
-			MethodName: "GetReview",
-			Handler:    _Middleware_GetReview_Handler,
+			MethodName: "Version",
+			Handler:    _Middleware_Version_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
