e8d8c4187992763d0d50c07c7fbfac9aaf42e4e3
Zhao KK <kikakkz@hotmail.com>
2023-10-13 05:29:55 +0000

    Remove unused api

--- npool/smoketest/mw/v1/module/module_grpc.pb.go
+++ npool/smoketest/mw/v1/module/module_grpc.pb.go
@@ -19,15 +19,14 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Middleware_CreateModule_FullMethodName      = "/smoketest.middleware.module.v1.Middleware/CreateModule"
-	Middleware_UpdateModule_FullMethodName      = "/smoketest.middleware.module.v1.Middleware/UpdateModule"
-	Middleware_GetModules_FullMethodName        = "/smoketest.middleware.module.v1.Middleware/GetModules"
-	Middleware_GetModule_FullMethodName         = "/smoketest.middleware.module.v1.Middleware/GetModule"
-	Middleware_GetModuleConds_FullMethodName    = "/smoketest.middleware.module.v1.Middleware/GetModuleConds"
-	Middleware_DeleteModule_FullMethodName      = "/smoketest.middleware.module.v1.Middleware/DeleteModule"
-	Middleware_ExistModule_FullMethodName       = "/smoketest.middleware.module.v1.Middleware/ExistModule"
-	Middleware_ExistModuleByName_FullMethodName = "/smoketest.middleware.module.v1.Middleware/ExistModuleByName"
-	Middleware_ExistModuleConds_FullMethodName  = "/smoketest.middleware.module.v1.Middleware/ExistModuleConds"
+	Middleware_CreateModule_FullMethodName     = "/smoketest.middleware.module.v1.Middleware/CreateModule"
+	Middleware_UpdateModule_FullMethodName     = "/smoketest.middleware.module.v1.Middleware/UpdateModule"
+	Middleware_GetModules_FullMethodName       = "/smoketest.middleware.module.v1.Middleware/GetModules"
+	Middleware_GetModule_FullMethodName        = "/smoketest.middleware.module.v1.Middleware/GetModule"
+	Middleware_GetModuleConds_FullMethodName   = "/smoketest.middleware.module.v1.Middleware/GetModuleConds"
+	Middleware_DeleteModule_FullMethodName     = "/smoketest.middleware.module.v1.Middleware/DeleteModule"
+	Middleware_ExistModule_FullMethodName      = "/smoketest.middleware.module.v1.Middleware/ExistModule"
+	Middleware_ExistModuleConds_FullMethodName = "/smoketest.middleware.module.v1.Middleware/ExistModuleConds"
 )
 
 // MiddlewareClient is the client API for Middleware service.
@@ -41,7 +40,6 @@ type MiddlewareClient interface {
 	GetModuleConds(ctx context.Context, in *GetModuleCondsRequest, opts ...grpc.CallOption) (*GetModuleCondsResponse, error)
 	DeleteModule(ctx context.Context, in *DeleteModuleRequest, opts ...grpc.CallOption) (*DeleteModuleResponse, error)
 	ExistModule(ctx context.Context, in *ExistModuleRequest, opts ...grpc.CallOption) (*ExistModuleResponse, error)
-	ExistModuleByName(ctx context.Context, in *ExistModuleByNameRequest, opts ...grpc.CallOption) (*ExistModuleByNameResponse, error)
 	ExistModuleConds(ctx context.Context, in *ExistModuleCondsRequest, opts ...grpc.CallOption) (*ExistModuleCondsResponse, error)
 }
 
@@ -116,15 +114,6 @@ func (c *middlewareClient) ExistModule(ctx context.Context, in *ExistModuleReque
 	return out, nil
 }
 
-func (c *middlewareClient) ExistModuleByName(ctx context.Context, in *ExistModuleByNameRequest, opts ...grpc.CallOption) (*ExistModuleByNameResponse, error) {
-	out := new(ExistModuleByNameResponse)
-	err := c.cc.Invoke(ctx, Middleware_ExistModuleByName_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 func (c *middlewareClient) ExistModuleConds(ctx context.Context, in *ExistModuleCondsRequest, opts ...grpc.CallOption) (*ExistModuleCondsResponse, error) {
 	out := new(ExistModuleCondsResponse)
 	err := c.cc.Invoke(ctx, Middleware_ExistModuleConds_FullMethodName, in, out, opts...)
@@ -145,7 +134,6 @@ type MiddlewareServer interface {
 	GetModuleConds(context.Context, *GetModuleCondsRequest) (*GetModuleCondsResponse, error)
 	DeleteModule(context.Context, *DeleteModuleRequest) (*DeleteModuleResponse, error)
 	ExistModule(context.Context, *ExistModuleRequest) (*ExistModuleResponse, error)
-	ExistModuleByName(context.Context, *ExistModuleByNameRequest) (*ExistModuleByNameResponse, error)
 	ExistModuleConds(context.Context, *ExistModuleCondsRequest) (*ExistModuleCondsResponse, error)
 	mustEmbedUnimplementedMiddlewareServer()
 }
@@ -174,9 +162,6 @@ func (UnimplementedMiddlewareServer) DeleteModule(context.Context, *DeleteModule
 }
 func (UnimplementedMiddlewareServer) ExistModule(context.Context, *ExistModuleRequest) (*ExistModuleResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExistModule not implemented")
-}
-func (UnimplementedMiddlewareServer) ExistModuleByName(context.Context, *ExistModuleByNameRequest) (*ExistModuleByNameResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ExistModuleByName not implemented")
 }
 func (UnimplementedMiddlewareServer) ExistModuleConds(context.Context, *ExistModuleCondsRequest) (*ExistModuleCondsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExistModuleConds not implemented")
@@ -320,24 +305,6 @@ func _Middleware_ExistModule_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Middleware_ExistModuleByName_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ExistModuleByNameRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(MiddlewareServer).ExistModuleByName(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: Middleware_ExistModuleByName_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(MiddlewareServer).ExistModuleByName(ctx, req.(*ExistModuleByNameRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 func _Middleware_ExistModuleConds_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ExistModuleCondsRequest)
 	if err := dec(in); err != nil {
@@ -390,10 +357,6 @@ var Middleware_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ExistModule",
 			Handler:    _Middleware_ExistModule_Handler,
-		},
-		{
-			MethodName: "ExistModuleByName",
-			Handler:    _Middleware_ExistModuleByName_Handler,
 		},
 		{
 			MethodName: "ExistModuleConds",
