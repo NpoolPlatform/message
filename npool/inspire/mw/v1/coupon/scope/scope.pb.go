2b7afef80381225148b1739b2517d99a60106bf1
lidaqiang <dqli161@163.com>
2023-11-01 17:39:13 +0800

    change to goodid1

--- npool/inspire/mw/v1/coupon/scope/scope.pb.go
+++ npool/inspire/mw/v1/coupon/scope/scope.pb.go
@@ -327,6 +327,7 @@ type Scope struct {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 789287843 (update scope)
 =======
@@ -366,6 +367,10 @@ type Scope struct {
 	// @inject_tag: sql:"good_id"
 	GoodID string `protobuf:"bytes,30,opt,name=GoodID,proto3" json:"GoodID,omitempty" sql:"good_id"`
 >>>>>>> b20bec5db (update scope)
+=======
+	// @inject_tag: sql:"good_id1"
+	GoodID string `protobuf:"bytes,30,opt,name=GoodID,proto3" json:"GoodID,omitempty" sql:"good_id1"`
+>>>>>>> 73d3e2dc4 (change to goodid1)
 	// @inject_tag: sql:"coupon_id"
 	CouponID string `protobuf:"bytes,40,opt,name=CouponID,proto3" json:"CouponID,omitempty" sql:"coupon_id"`
 	// @inject_tag: sql:"coupon_name"
