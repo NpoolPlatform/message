d451fb8db842601cc5bdac646a6381a6a8edfd61
Zhao KK <kikakkz@hotmail.com>
2023-11-08 07:38:12 +0000

    Add ID to conds

--- npool/appuser/mw/v1/kyc/kyc.pb.go
+++ npool/appuser/mw/v1/kyc/kyc.pb.go
@@ -30,6 +30,7 @@ type KycReq struct {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	ID           *uint32             `protobuf:"varint,9,opt,name=ID,proto3,oneof" json:"ID,omitempty"`
 =======
 >>>>>>> 8eb981ff1 (Add auto increment id)
@@ -41,6 +42,9 @@ type KycReq struct {
 =======
 >>>>>>> 696b80d22 (Add auto increment id)
 >>>>>>> 12ef4547d (Add auto increment id)
+=======
+	ID           *uint32             `protobuf:"varint,9,opt,name=ID,proto3,oneof" json:"ID,omitempty"`
+>>>>>>> 320847e56 (Add ID to conds)
 	EntID        *string             `protobuf:"bytes,10,opt,name=EntID,proto3,oneof" json:"EntID,omitempty"`
 	AppID        *string             `protobuf:"bytes,20,opt,name=AppID,proto3,oneof" json:"AppID,omitempty"`
 	UserID       *string             `protobuf:"bytes,30,opt,name=UserID,proto3,oneof" json:"UserID,omitempty"`
@@ -89,6 +93,7 @@ func (*KycReq) Descriptor() ([]byte, []int) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 12ef4547d (Add auto increment id)
 func (x *KycReq) GetID() uint32 {
@@ -108,6 +113,11 @@ func (x *KycReq) GetID() uint32 {
 =======
 >>>>>>> 696b80d22 (Add auto increment id)
 >>>>>>> 12ef4547d (Add auto increment id)
+=======
+func (x *KycReq) GetID() uint32 {
+	if x != nil && x.ID != nil {
+		return *x.ID
+>>>>>>> 320847e56 (Add ID to conds)
 	}
 	return 0
 }
@@ -427,7 +437,6 @@ type Conds struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-<<<<<<< HEAD
 	ID           *v1.Uint32Val `protobuf:"bytes,9,opt,name=ID,proto3,oneof" json:"ID,omitempty"`
 	EntID        *v1.StringVal `protobuf:"bytes,10,opt,name=EntID,proto3,oneof" json:"EntID,omitempty"`
 	AppID        *v1.StringVal `protobuf:"bytes,20,opt,name=AppID,proto3,oneof" json:"AppID,omitempty"`
@@ -437,16 +446,6 @@ type Conds struct {
 	DocumentType *v1.Uint32Val `protobuf:"bytes,60,opt,name=DocumentType,proto3,oneof" json:"DocumentType,omitempty"`
 	EntityType   *v1.Uint32Val `protobuf:"bytes,70,opt,name=EntityType,proto3,oneof" json:"EntityType,omitempty"`
 	State        *v1.Uint32Val `protobuf:"bytes,80,opt,name=State,proto3,oneof" json:"State,omitempty"`
-=======
-	EntID        *v1.StringVal `protobuf:"bytes,10,opt,name=EntID,proto3" json:"EntID,omitempty"`
-	AppID        *v1.StringVal `protobuf:"bytes,20,opt,name=AppID,proto3" json:"AppID,omitempty"`
-	UserID       *v1.StringVal `protobuf:"bytes,30,opt,name=UserID,proto3" json:"UserID,omitempty"`
-	ReviewID     *v1.StringVal `protobuf:"bytes,40,opt,name=ReviewID,proto3" json:"ReviewID,omitempty"`
-	IDNumber     *v1.StringVal `protobuf:"bytes,50,opt,name=IDNumber,proto3" json:"IDNumber,omitempty"`
-	DocumentType *v1.Uint32Val `protobuf:"bytes,60,opt,name=DocumentType,proto3" json:"DocumentType,omitempty"`
-	EntityType   *v1.Uint32Val `protobuf:"bytes,70,opt,name=EntityType,proto3" json:"EntityType,omitempty"`
-	State        *v1.Uint32Val `protobuf:"bytes,80,opt,name=State,proto3" json:"State,omitempty"`
->>>>>>> 696b80d22 (Add auto increment id)
 }
 
 func (x *Conds) Reset() {
@@ -481,13 +480,9 @@ func (*Conds) Descriptor() ([]byte, []int) {
 	return file_npool_appuser_mw_v1_kyc_kyc_proto_rawDescGZIP(), []int{2}
 }
 
-<<<<<<< HEAD
 func (x *Conds) GetID() *v1.Uint32Val {
-=======
-func (x *Conds) GetEntID() *v1.StringVal {
->>>>>>> 696b80d22 (Add auto increment id)
 	if x != nil {
-		return x.EntID
+		return x.ID
 	}
 	return nil
 }
