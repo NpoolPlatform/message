38bf66ead6f980e2a7f54dd753a8e41e4aea2520
Zhao KK <kikakkz@hotmail.com>
2024-04-11 06:33:30 +0000

    Add required

--- npool/good/gw/v1/good/required/required_grpc.pb.go
+++ npool/good/gw/v1/good/required/required_grpc.pb.go
@@ -19,20 +19,20 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Gateway_CreateRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/CreateRequired"
-	Gateway_UpdateRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/UpdateRequired"
-	Gateway_GetRequireds_FullMethodName   = "/good.gateway.good1.required1.v1.Gateway/GetRequireds"
-	Gateway_DeleteRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/DeleteRequired"
+	Gateway_AdminCreateRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/AdminCreateRequired"
+	Gateway_AdminUpdateRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/AdminUpdateRequired"
+	Gateway_GetRequireds_FullMethodName        = "/good.gateway.good1.required1.v1.Gateway/GetRequireds"
+	Gateway_AdminDeleteRequired_FullMethodName = "/good.gateway.good1.required1.v1.Gateway/AdminDeleteRequired"
 )
 
 // GatewayClient is the client API for Gateway service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type GatewayClient interface {
-	CreateRequired(ctx context.Context, in *CreateRequiredRequest, opts ...grpc.CallOption) (*CreateRequiredResponse, error)
-	UpdateRequired(ctx context.Context, in *UpdateRequiredRequest, opts ...grpc.CallOption) (*UpdateRequiredResponse, error)
+	AdminCreateRequired(ctx context.Context, in *AdminCreateRequiredRequest, opts ...grpc.CallOption) (*AdminCreateRequiredResponse, error)
+	AdminUpdateRequired(ctx context.Context, in *AdminUpdateRequiredRequest, opts ...grpc.CallOption) (*AdminUpdateRequiredResponse, error)
 	GetRequireds(ctx context.Context, in *GetRequiredsRequest, opts ...grpc.CallOption) (*GetRequiredsResponse, error)
-	DeleteRequired(ctx context.Context, in *DeleteRequiredRequest, opts ...grpc.CallOption) (*DeleteRequiredResponse, error)
+	AdminDeleteRequired(ctx context.Context, in *AdminDeleteRequiredRequest, opts ...grpc.CallOption) (*AdminDeleteRequiredResponse, error)
 }
 
 type gatewayClient struct {
@@ -43,18 +43,18 @@ func NewGatewayClient(cc grpc.ClientConnInterface) GatewayClient {
 	return &gatewayClient{cc}
 }
 
-func (c *gatewayClient) CreateRequired(ctx context.Context, in *CreateRequiredRequest, opts ...grpc.CallOption) (*CreateRequiredResponse, error) {
-	out := new(CreateRequiredResponse)
-	err := c.cc.Invoke(ctx, Gateway_CreateRequired_FullMethodName, in, out, opts...)
+func (c *gatewayClient) AdminCreateRequired(ctx context.Context, in *AdminCreateRequiredRequest, opts ...grpc.CallOption) (*AdminCreateRequiredResponse, error) {
+	out := new(AdminCreateRequiredResponse)
+	err := c.cc.Invoke(ctx, Gateway_AdminCreateRequired_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *gatewayClient) UpdateRequired(ctx context.Context, in *UpdateRequiredRequest, opts ...grpc.CallOption) (*UpdateRequiredResponse, error) {
-	out := new(UpdateRequiredResponse)
-	err := c.cc.Invoke(ctx, Gateway_UpdateRequired_FullMethodName, in, out, opts...)
+func (c *gatewayClient) AdminUpdateRequired(ctx context.Context, in *AdminUpdateRequiredRequest, opts ...grpc.CallOption) (*AdminUpdateRequiredResponse, error) {
+	out := new(AdminUpdateRequiredResponse)
+	err := c.cc.Invoke(ctx, Gateway_AdminUpdateRequired_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -70,9 +70,9 @@ func (c *gatewayClient) GetRequireds(ctx context.Context, in *GetRequiredsReques
 	return out, nil
 }
 
-func (c *gatewayClient) DeleteRequired(ctx context.Context, in *DeleteRequiredRequest, opts ...grpc.CallOption) (*DeleteRequiredResponse, error) {
-	out := new(DeleteRequiredResponse)
-	err := c.cc.Invoke(ctx, Gateway_DeleteRequired_FullMethodName, in, out, opts...)
+func (c *gatewayClient) AdminDeleteRequired(ctx context.Context, in *AdminDeleteRequiredRequest, opts ...grpc.CallOption) (*AdminDeleteRequiredResponse, error) {
+	out := new(AdminDeleteRequiredResponse)
+	err := c.cc.Invoke(ctx, Gateway_AdminDeleteRequired_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -83,10 +83,10 @@ func (c *gatewayClient) DeleteRequired(ctx context.Context, in *DeleteRequiredRe
 // All implementations must embed UnimplementedGatewayServer
 // for forward compatibility
 type GatewayServer interface {
-	CreateRequired(context.Context, *CreateRequiredRequest) (*CreateRequiredResponse, error)
-	UpdateRequired(context.Context, *UpdateRequiredRequest) (*UpdateRequiredResponse, error)
+	AdminCreateRequired(context.Context, *AdminCreateRequiredRequest) (*AdminCreateRequiredResponse, error)
+	AdminUpdateRequired(context.Context, *AdminUpdateRequiredRequest) (*AdminUpdateRequiredResponse, error)
 	GetRequireds(context.Context, *GetRequiredsRequest) (*GetRequiredsResponse, error)
-	DeleteRequired(context.Context, *DeleteRequiredRequest) (*DeleteRequiredResponse, error)
+	AdminDeleteRequired(context.Context, *AdminDeleteRequiredRequest) (*AdminDeleteRequiredResponse, error)
 	mustEmbedUnimplementedGatewayServer()
 }
 
@@ -94,17 +94,17 @@ type GatewayServer interface {
 type UnimplementedGatewayServer struct {
 }
 
-func (UnimplementedGatewayServer) CreateRequired(context.Context, *CreateRequiredRequest) (*CreateRequiredResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateRequired not implemented")
+func (UnimplementedGatewayServer) AdminCreateRequired(context.Context, *AdminCreateRequiredRequest) (*AdminCreateRequiredResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AdminCreateRequired not implemented")
 }
-func (UnimplementedGatewayServer) UpdateRequired(context.Context, *UpdateRequiredRequest) (*UpdateRequiredResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateRequired not implemented")
+func (UnimplementedGatewayServer) AdminUpdateRequired(context.Context, *AdminUpdateRequiredRequest) (*AdminUpdateRequiredResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AdminUpdateRequired not implemented")
 }
 func (UnimplementedGatewayServer) GetRequireds(context.Context, *GetRequiredsRequest) (*GetRequiredsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetRequireds not implemented")
 }
-func (UnimplementedGatewayServer) DeleteRequired(context.Context, *DeleteRequiredRequest) (*DeleteRequiredResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DeleteRequired not implemented")
+func (UnimplementedGatewayServer) AdminDeleteRequired(context.Context, *AdminDeleteRequiredRequest) (*AdminDeleteRequiredResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AdminDeleteRequired not implemented")
 }
 func (UnimplementedGatewayServer) mustEmbedUnimplementedGatewayServer() {}
 
@@ -119,38 +119,38 @@ func RegisterGatewayServer(s grpc.ServiceRegistrar, srv GatewayServer) {
 	s.RegisterService(&Gateway_ServiceDesc, srv)
 }
 
-func _Gateway_CreateRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateRequiredRequest)
+func _Gateway_AdminCreateRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AdminCreateRequiredRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GatewayServer).CreateRequired(ctx, in)
+		return srv.(GatewayServer).AdminCreateRequired(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Gateway_CreateRequired_FullMethodName,
+		FullMethod: Gateway_AdminCreateRequired_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GatewayServer).CreateRequired(ctx, req.(*CreateRequiredRequest))
+		return srv.(GatewayServer).AdminCreateRequired(ctx, req.(*AdminCreateRequiredRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Gateway_UpdateRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UpdateRequiredRequest)
+func _Gateway_AdminUpdateRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AdminUpdateRequiredRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GatewayServer).UpdateRequired(ctx, in)
+		return srv.(GatewayServer).AdminUpdateRequired(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Gateway_UpdateRequired_FullMethodName,
+		FullMethod: Gateway_AdminUpdateRequired_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GatewayServer).UpdateRequired(ctx, req.(*UpdateRequiredRequest))
+		return srv.(GatewayServer).AdminUpdateRequired(ctx, req.(*AdminUpdateRequiredRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -173,20 +173,20 @@ func _Gateway_GetRequireds_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Gateway_DeleteRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DeleteRequiredRequest)
+func _Gateway_AdminDeleteRequired_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AdminDeleteRequiredRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GatewayServer).DeleteRequired(ctx, in)
+		return srv.(GatewayServer).AdminDeleteRequired(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Gateway_DeleteRequired_FullMethodName,
+		FullMethod: Gateway_AdminDeleteRequired_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GatewayServer).DeleteRequired(ctx, req.(*DeleteRequiredRequest))
+		return srv.(GatewayServer).AdminDeleteRequired(ctx, req.(*AdminDeleteRequiredRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -199,20 +199,20 @@ var Gateway_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*GatewayServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "CreateRequired",
-			Handler:    _Gateway_CreateRequired_Handler,
+			MethodName: "AdminCreateRequired",
+			Handler:    _Gateway_AdminCreateRequired_Handler,
 		},
 		{
-			MethodName: "UpdateRequired",
-			Handler:    _Gateway_UpdateRequired_Handler,
+			MethodName: "AdminUpdateRequired",
+			Handler:    _Gateway_AdminUpdateRequired_Handler,
 		},
 		{
 			MethodName: "GetRequireds",
 			Handler:    _Gateway_GetRequireds_Handler,
 		},
 		{
-			MethodName: "DeleteRequired",
-			Handler:    _Gateway_DeleteRequired_Handler,
+			MethodName: "AdminDeleteRequired",
+			Handler:    _Gateway_AdminDeleteRequired_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
